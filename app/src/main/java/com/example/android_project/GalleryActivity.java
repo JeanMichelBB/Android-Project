3061e2c31f245781119cbb95e3554d78e80e74f6
JeanMichelBB <jeanmichelbberube@outlook.com>
2023-09-25 17:20:48 -0400

    Login,Registration,GalleryActivity

---
+++ app/src/main/java/com/example/android_project/GalleryActivity.java
@@ -0,0 +1,16 @@
+package com.example.android_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Surface;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+
+}
