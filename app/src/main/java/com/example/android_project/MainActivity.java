fa23828a55679a28633de96f476d6e790a83ea17
JeanMichelBB <jeanmichelbberube@outlook.com>
2023-10-03 12:34:12 -0400

    ipatest

--- app/src/main/java/com/example/android_project/MainActivity.java
+++ app/src/main/java/com/example/android_project/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.android_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.android_project.data.ApiBroker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView resultTextView = findViewById(R.id.textView);
+
+        String apiUrl = "https://api.themoviedb.org/3/movie/550?api_key=16338f6f183f5ee2fce0e99d55cfbb5f";
+
+        ApiBroker.fetchData(apiUrl, new ApiBroker.ApiResponseListener() {
+            @Override
+            public void onApiResponse(String response) {
+                resultTextView.setText(response);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android_project/data/ApiBroker.java
+++ app/src/main/java/com/example/android_project/data/ApiBroker.java
@@ -1,5 +1,71 @@
 package com.example.android_project.data;
 
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ApiBroker {
+    public interface ApiResponseListener {
+        void onApiResponse(String response);
+    }
+    public static void fetchData(String apiUrl, ApiResponseListener listener) {
+        new FetchDataTask(apiUrl, listener).execute();
+    }
+    private static class FetchDataTask extends AsyncTask<Void, Void, String> {
+        private final String apiUrl;
+        private final ApiResponseListener listener;
+
+        FetchDataTask(String apiUrl, ApiResponseListener listener) {
+            this.apiUrl = apiUrl;
+            this.listener = listener;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL(apiUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // Set up the connection
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                // Get the response
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    response.append(line);
+                }
+
+                // Close the streams and disconnect the connection
+                bufferedReader.close();
+                inputStream.close();
+                connection.disconnect();
+
+                return response.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String response) {
+            if (response != null) {
+                listener.onApiResponse(response);
+            } else {
+                // Handle error
+                listener.onApiResponse("Error fetching data from API");
+            }
+        }
+    }
 
 }
