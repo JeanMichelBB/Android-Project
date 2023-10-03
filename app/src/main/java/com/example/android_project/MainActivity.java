6dfc2d9a471180da7fc0a394035db615cae45ad3
JeanMichelBB <jeanmichelbberube@outlook.com>
2023-10-03 19:45:08 -0400

    testurlimage

--- app/src/main/java/com/example/android_project/MainActivity.java
+++ app/src/main/java/com/example/android_project/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android_project.data.ApiBroker;
@@ -15,11 +16,24 @@ import org.json.JSONObject;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView resultTextView = findViewById(R.id.textView);
+
+        String apiUrl = "https://api.themoviedb.org/3/movie/550?api_key=16338f6f183f5ee2fce0e99d55cfbb5f";
+
+        ApiBroker.fetchData(apiUrl, new ApiBroker.ApiResponseListener() {
+            @Override
+            public void onApiResponse(String response) {
+                resultTextView.setText(response);
+            }
+        });
     }
 }
