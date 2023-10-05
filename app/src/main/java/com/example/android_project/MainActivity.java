735c8ee442c0b8281ebacceb5061855782754680
JeanMichelBB <jeanmichelbberube@outlook.com>
2023-10-04 20:06:31 -0400

    show a image from internet

--- app/src/main/java/com/example/android_project/MainActivity.java
+++ app/src/main/java/com/example/android_project/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android_project.data.ApiBroker;
 import com.example.android_project.models.MovieModel;
@@ -13,28 +18,42 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imageView;
+    String poster_path = "/hek3koDUyRQk7FIhPXsa6mT2Zc3.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView resultTextView = findViewById(R.id.textView);
-
-        String apiUrl = "https://api.themoviedb.org/3/movie/550?api_key=16338f6f183f5ee2fce0e99d55cfbb5f";
-
-        ApiBroker.fetchData(apiUrl, new ApiBroker.ApiResponseListener() {
-            @Override
-            public void onApiResponse(String response) {
-                resultTextView.setText(response);
+        new DownloadImageFromInternet((ImageView) findViewById(R.id.imageView)).execute("https://image.tmdb.org/t/p/w500"+poster_path);
+    }
+    private class DownloadImageFromInternet extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+        public DownloadImageFromInternet(ImageView imageView) {
+            this.imageView=imageView;
+            Toast.makeText(getApplicationContext(), "Please wait, it may take a few minute...",Toast.LENGTH_SHORT).show();
+        }
+        protected Bitmap doInBackground(String... urls) {
+            String imageURL=urls[0];
+            Bitmap bimage=null;
+            try {
+                InputStream in=new java.net.URL(imageURL).openStream();
+                bimage= BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error Message", e.getMessage());
+                e.printStackTrace();
             }
-        });
-        imageView = findViewById(R.id.imageView);
+            return bimage;
+        }
+        protected void onPostExecute(Bitmap result) {
+            imageView.setImageBitmap(result);
+        }
     }
 }
+
