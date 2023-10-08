212d9080aba4bf44d8852613fe04f0554d75ac0c
JeanMichelBB <jeanmichelbberube@outlook.com>
2023-10-07 23:31:15 -0400

    User List not working

--- app/src/main/java/com/example/android_project/business/MovieManagement.java
+++ app/src/main/java/com/example/android_project/business/MovieManagement.java
@@ -3,6 +3,7 @@ package com.example.android_project.business;
 import com.example.android_project.data.ApplicationDB;
 import com.example.android_project.models.MovieModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MovieManagement {
@@ -10,7 +11,7 @@ public class MovieManagement {
     public MovieManagement(){
         this.applicationDB = new ApplicationDB();
     }
-    public List<MovieModel> getListOfMovies(){
+    public ArrayList<MovieModel> getAllMovies(){
         return applicationDB.getAllMovies();
     }
     public void assignMovie(MovieModel movie){
--- app/src/main/java/com/example/android_project/view/HomeUserActivity.java
+++ app/src/main/java/com/example/android_project/view/HomeUserActivity.java
@@ -5,31 +5,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.android_project.R;
 import com.example.android_project.business.Authentication;
+import com.example.android_project.business.MovieManagement;
+import com.example.android_project.models.MovieModel;
 import com.example.android_project.models.UserModel;
 
 import java.util.ArrayList;
 
 public class HomeUserActivity extends AppCompatActivity {
     TextView txtWelcome;
+    ListView listView;
     UserModel user;
-
-//    ArrayList<MovieModel> movieList = new ArrayList<>();
-//    ListView listViewMovies;
+    MovieManagement movieManagement = new MovieManagement();
+    ArrayList<MovieModel> movieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_user);
-
         user = (UserModel) getIntent().getSerializableExtra("AUTH_USER");
-
         txtWelcome = findViewById(R.id.txtWelcome);
+        listView = findViewById(R.id.listView);
 
         txtWelcome.setText("Welcome " + user.getFullName());
+
+        movieList = movieManagement.getAllMovies();
+        MovieAdapter adapter = new MovieAdapter(HomeUserActivity.this, movieList, false);
+        listView.setAdapter(adapter);
     }
 
     @Override
